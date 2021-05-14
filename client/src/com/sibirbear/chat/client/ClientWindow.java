e1cf53951b601ebac3f6df644782112e98fe3cdc
SibirBear <test@test.com>
2021-05-14 22:28:23 +0700

    refactor ClientWindow (lambda) in module client

--- client/src/com/sibirbear/chat/client/ClientWindow.java
+++ client/src/com/sibirbear/chat/client/ClientWindow.java
@@ -17,12 +17,7 @@ public class ClientWindow extends JFrame implements ActionListener, ITCPConnecti
     public static final int HEIGHT = 400;
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ClientWindow();
-            }
-        });
+        SwingUtilities.invokeLater(ClientWindow::new);
     }
 
     private final JTextArea log = new JTextArea();
@@ -83,12 +78,9 @@ public class ClientWindow extends JFrame implements ActionListener, ITCPConnecti
     }
 
     private synchronized void printMsg(String msg) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                log.append(msg + "\n");
-                log.setCaretPosition(log.getDocument().getLength());
-            }
+        SwingUtilities.invokeLater(() -> {
+            log.append(msg + "\n");
+            log.setCaretPosition(log.getDocument().getLength());
         });
     }
 
